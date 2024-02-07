f33c2e33dc947493830c3a1be571e5e8782ea440
mausor11 <bartosz.danko@gmail.com>
2024-02-07 23:07:36 +0100

    loginScreen

---
+++ app/src/main/java/com/example/isodnotify/activities/LoginScreen.kt
@@ -0,0 +1,12 @@
+package com.example.isodnotify.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.isodnotify.R
+
+class LoginScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_screen)
+    }
+}
